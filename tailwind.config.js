1142b104756b19803be0df5a84cf36435febc1bf
unknown <ryle_andrei_cabudol@dlsl.edu.ph>
2024-08-07 02:12:50 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cyan: '#66FCF1',
+        dwhite: '#FFFFFF',
+        bblack: '#0B0C10'
+      }
+    },
+  },
+  plugins: [],
+}
+
