10fa003252dfe38ab31f5d0147bd0f67a602b953
unknown <ryle_andrei_cabudol@dlsl.edu.ph>
2024-08-08 00:20:38 +0800

    Main page almost done

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ module.exports = {
       colors: {
         cyan: '#66FCF1',
         dwhite: '#FFFFFF',
-        bblack: '#0B0C10'
+        bblack: '#0B0C10',
+        dblue: '#000033',
+        ggray: '#323031'
       }
     },
   },
